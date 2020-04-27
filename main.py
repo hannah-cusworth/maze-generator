c15c590303ce043333b3eda2ec3137121e6fc493
Bewick <hanncus430@gmail.com>
2020-04-27 18:27:38 +0100

    current cell moves randomly.

--- main.py
+++ main.py
@@ -4,27 +4,28 @@ import pygame
 from pygame.locals import *
 import window
 import algorithms
+import random
 
 pygame.init()
 # Dimensions
 screenx = 2500
 screeny = 1500
 
-grid_size = 20
-border_width = 40 # note that because the border grows either side of line, only half of this is visible
+grid_size = 50
+border_width = grid_size*2 # note that because the border grows either side of line, only half of this is visible
 
 # Colours
 background_colour = (255,255,255)
 grid_colour = (0,0,0)
 border_colour = (255, 0, 0)
-cell_colour = (0,0,255)
+cell_colour = (160,160,160)
     
 def draw_grid(background):
 
-    for x in range(0, screenx, grid_size):
+    for x in range(0, screenx, grid_size + 1):
         pygame.draw.line(background, grid_colour, (x, 0), (x, screeny), 1)
         
-    for y in range(0, screeny, grid_size):
+    for y in range(0, screeny, grid_size + 1):
         pygame.draw.line(background, grid_colour, (0, y), (screenx, y), 1)
     
     # Draw border
@@ -32,14 +33,27 @@ def draw_grid(background):
     pygame.draw.rect(background, border_colour, border, border_width)
 
 class Cell():
-    def __init__(self, start):
-        self.start = start
-        self.dirnx = 0
-        self.dirny = 0
+    def __init__(self, x, y):
+        self.start = (x,y)
+        self.coord = [x,y]
+        self.rect = Rect(self.coord[0], self.coord[1], grid_size, grid_size)
         self.colour = cell_colour
+    def move(self):
+        directions = {
+            "North": (0, grid_size + 1),
+            "South": (0, -grid_size - 1), 
+            "East": (grid_size + 1, 0), 
+            "West": (-grid_size - 1, 0)
+            }
+        move = random.choice(list(directions.items()))
+        print(move[1])
+        self.rect.move_ip(move[1])
+        
+        
+    
+        
 
-    def draw(self, background):
-        pygame.draw.rect(background, self.colour, self.start)
+
 
 def main():
     pygame.init()
@@ -54,9 +68,12 @@ def main():
     draw_grid(background)
 
     # Create cell
-    start = Rect(300,300,20,20)
-    current = Cell(start)
-    current.draw(background)
+    current = Cell(511,1021)
+    background.fill(current.colour, rect=current.rect)
+    for i in range(50):
+        current.move()
+        background.fill(current.colour, rect=current.rect)
+        print(current.rect)
 
     # Blit everything to the screen
     screen.blit(background, (0, 0))
