c4ef8465e5464aa496c54279e041279b8b1e41f6
Bewick <hanncus430@gmail.com>
2020-05-08 15:06:39 +0100

    Added algorithm descriptions which change with dropdown.

--- window.py
+++ window.py
@@ -7,6 +7,10 @@ algorithm_list = [
             "Eller's Algorithm",
             "Algorithm 3",
             ]
+algo_info = [
+    "Blah",
+    "foo",
+]
 
 # Padding
 x = 50
@@ -21,7 +25,7 @@ class AlgoSelectWindow():
         # Configure window
         self.window.title("Maze Generator")
         self.window.columnconfigure(0, minsize=500, weight=1)
-        self.window.rowconfigure([0,1,2], minsize=100, weight=1)
+        self.window.rowconfigure([0,1,2,3], minsize=100, weight=1)
 
         # Set font
         myfont = tkFont.Font(family="Helvetica", size=48)
@@ -36,7 +40,7 @@ class AlgoSelectWindow():
             text="Welcome to the maze generation alogrithm visualizer!",
             font=myfont,
             padx=x,
-            pady=y
+            pady=y,
         )
         greeting.pack()
        
@@ -58,7 +62,10 @@ class AlgoSelectWindow():
         select_widget = tk.OptionMenu(     # OptionMenu(parent, variable, value)
             select_frame, 
             self.select_variable,
-            *algorithm_list,) 
+            *algorithm_list,
+            ) 
+        self.select_variable.trace("w", self.change_info_text)
+
         select_widget.configure(
             font=listfont, 
             width=20, 
@@ -68,6 +75,19 @@ class AlgoSelectWindow():
         menu = select_widget.nametowidget(select_widget.menuname)     # Get dropdown menu
         menu.configure(font=(listfont))                                     # Set font size of menu
         select_widget.pack()
+
+        # Information frame
+        info_frame = tk.Frame()
+        info_frame.grid(row=2, column=0)
+        self.info_text_variable = tk.StringVar(self.window)
+        self.info_text = tk.Label(
+            master=info_frame,
+            textvariable=self.info_text_variable,
+            font=myfont,
+            padx=x,
+            pady=y,
+        )
+        self.info_text.pack()
 
         # Button
         run_button = tk.Button(
@@ -81,7 +101,7 @@ class AlgoSelectWindow():
             pady=10,
         )
         run_button.grid(
-            row=2, 
+            row=3, 
             column=0,
             padx=x,
             pady=button_padding,
@@ -95,7 +115,15 @@ class AlgoSelectWindow():
         if self.choice:
             self.quit()
             
-    
+    def change_info_text(self, foo, bar, baz): # Dummy args
+        choice = self.select_variable.get()
+        if choice == algorithm_list[0]:
+            self.info_text_variable.set(algo_info[0])
+        if choice == algorithm_list[1]:
+            self.info_text_variable.set(algo_info[1])
+
+
+
     def quit(self):
         self.window.destroy()
 
